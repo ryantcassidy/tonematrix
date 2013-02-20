aa3d517bc62080a04f79032b6f3a2a4751755b13
Ryan T. Cassidy <rcassidy@alleyrally.ccs.neu.edu>
2013-02-20 17:49:14 -0500

    WOAH DUDE.

--- raytrace/raytracer.py
+++ raytrace/raytracer.py
@@ -9,14 +9,12 @@ args = sys.argv
 scene = args[1]
 
 class Light:
-
 	value = []
 
 	def __init__(self,value):
 		self.value = value
 
 class Point(Light):
-
 	position = None
 	normal = None
 
@@ -26,7 +24,6 @@ class Point(Light):
 		self.normal = n
 
 class Directional(Light):
-
 	position = None
 	normal = None
 
@@ -36,7 +33,6 @@ class Directional(Light):
 		self.normal = n
 
 class Spot(Light):
-
 	position = None
 	normal = None
 
@@ -46,33 +42,27 @@ class Spot(Light):
 		self.normal = n
 
 class lAmbient(Light):
-
 	pass
 
 class Material:
-
 	value = []
 
 	def __init__(self,value):
 		self.value = value
 
 class mAmbient(Material):
-
 	def __init__(self,value=[0.2, 0.2, 0.2]):
 		Material.__init__(self,value)
 
 class Diffuse(Material):
-
 	def __init__(self,value=[1, 1, 1]):
 		Material.__init__(self,value)
 
 class Specular(Material):
-
 	def __init__(self,value=[1, 1, 1, 64]):
 		Material.__init__(self,value)
 
 class Transmissive(Material):
-
 	def __init__(self,value=[0, 0, 0, 1]):
 		Material.__init__(self,value)
 
@@ -93,7 +83,6 @@ class Sphere:
 		self.transmissiveMaterial = tm
 
 class Vertex:
-
 	position = []
 	normal = []
 
@@ -102,7 +91,6 @@ class Vertex:
 		self.normal   = [value[3], value[4], value[5]]
 
 class Camera:
-
 	position = None
 	normal = None
 	fustrum = None
@@ -113,7 +101,6 @@ class Camera:
 		self.fustrum = f
 
 class Ray:
-
 	position = None
 	vector = None
 
@@ -144,7 +131,6 @@ class Raytracer:
 		self.fileName = scene
 
 	def parseFile(self):
-
 		file = open(self.fileName)
 
 		for line in file:
@@ -223,6 +209,11 @@ class Raytracer:
 	def norm(self, v): 
 		return [x/math.sqrt(self.dot(v,v)) for x in v]
 
+	def cross(self, v1, v2):
+		return [v1[1] * v2[2] - v2[1] * v1[2],
+		        v1[2] * v2[0] - v2[2] * v1[0],
+						v1[0] * v2[1] - v2[0] * v1[1]]
+
 	def doIt(self):
 		start = int(round(time.time() * 1000))
 		width = self.imageResolution[0]
@@ -235,7 +226,6 @@ class Raytracer:
 
 		for x in range(width):
 			for y in range(height):
-
 				# RIGHT, LEFT, TOP, BOTTOM. center of image x-y plane is 0,0.
 				r = width/2
 				l = -1 * r
@@ -252,33 +242,34 @@ class Raytracer:
 				# D = distance to frame
 				d = width/(2*math.tan(theta))
 
-				# direction vector of camera (unit)
-				look = self.norm(camera.normal[:])
-
 				# up is always <0,1,0>, unit
-				up = [0,1,0]
+				up_v = [0,1,0]
 
-				# right vector is perpendicular to up, look
-				right = [(look[1] * up[2]) - (up[1] * look[2]),
-						 (look[2] * up[0]) - (up[2] * look[0]),
-						 (look[0] * up[1]) - (up[0] * look[1])]
-
-				right = self.norm(right)
+				# direction vector of camera (unit) = -W
+				W = self.norm(camera.normal[:])
+				U = self.norm(self.cross(up_v, W))
+				V = self.cross(W, U)
 
 				# scale the look vector by distance
+				look = W[:]
 				look[0] *= d
 				look[1] *= d
 				look[2] *= d
 
 				# scale right vector by V coord
-				right[0] *= v
-				right[1] *= v
-				right[2] *= v
+				right = U[:]
+				right[0] *= u
+				right[1] *= u
+				right[2] *= u
 
 				# scale up vector by U coord
-				up = [0,u,0]
+				up = V[:]
+				up[0] *= v 
+				up[1] *= v 
+				up[2] *= v 
 
 				# ray direction vector is all these combined
+			  # -w * d + up * U + right * V
 				rayDirection = [look[0] + up[0] + right[0],
 								look[1] + up[1] + right[1],
 								look[2] + up[2] + right[2]]
@@ -288,8 +279,9 @@ class Raytracer:
 				
 				# trace the ray
 				result = self.traceRay(ray,self.lights)
-
-				pixels[x,y] = result
+				result_tuple = (result[0]* 255, result[1] * 255, result[2] * 255)
+				print result_tuple
+				pixels[x,y] = result_tuple
 
 
 		image.save(self.outputImage + ".gif")
@@ -301,84 +293,15 @@ class Raytracer:
 
 		# for every sphere
 		for s in self.spheres:
-			# mat = s.ambientMaterial.value
-			# results.append((self.traceSphere(ray, s), (int(mat[0]*255),int(mat[1]*255),int(mat[2]*255))))
-
 			# check if intersect with sphere
-			t = self.traceSphere(ray,s)
-			if t:
+			results.append(self.traceSphere(ray,s))
 
-				rayVector = self.norm(ray.vector)
-
-				pointOnSphere = ray.position[:]
-				pointOnSphere[0] += rayVector[0] * t
-				pointOnSphere[1] += rayVector[1] * t
-				pointOnSphere[2] += rayVector[2] * t
-
-				# print pointOnSphere
-
-				pointNormal = pointOnSphere[:]
-				pointNormal[0] -= s.position[0]
-				pointNormal[1] -= s.position[1]
-				pointNormal[2] -= s.position[2]
-
-				pointLight = None
-				for light in lights:
-					if light.__class__.__name__ == 'Point':
-						pointLight = light
-						break
-
-				pointLightNormal = self.norm(self.sub(pointOnSphere,pointLight.position))
-				pointNormal = self.norm(pointNormal)
-
-				tempDot = numpy.dot(pointNormal,pointLightNormal)
-				surfaceTangent = max(0,tempDot)
-
-				diffuseRed = s.diffuseMaterial.value[0]*255
-				ambientRed = s.ambientMaterial.value[0]*255
-				red = (diffuseRed + ambientRed)/2
-				pixelRed = red * pointLight.value[0] * surfaceTangent
-
-				diffuseGreen = s.diffuseMaterial.value[1]*255
-				ambientGreen = s.ambientMaterial.value[1]*255
-				green = (diffuseGreen + ambientGreen)/2
-				pixelGreen = green * pointLight.value[1] * surfaceTangent
-
-				diffuseBlue = s.diffuseMaterial.value[2]*255
-				ambientBlue = s.ambientMaterial.value[2]*255
-				blue = (diffuseBlue + ambientBlue)/2
-				pixelBlue = blue * pointLight.value[2] * surfaceTangent
-				colors = (int(pixelRed),int(pixelGreen),int(pixelBlue))
-			
-				# print pointLight.value
-				# print surfaceTangent
-				# print (pixelRed,pixelGreen,pixelBlue)
-				# print colors
-
-
-				# print "Diffuse"
-				# print diffuseRed
-				# print diffuseGreen
-				# print diffuseBlue
-				# print "Pixel"
-				# print pixelRed
-				# print pixelGreen
-				# print pixelBlue
-				# print "Point Light"
-				# print pointLight.value[0]
-				# print pointLight.value[1]
-				# print pointLight.value[2]
-
-				results.append((t,colors))
-
-		results = filter(lambda tuple: tuple[0], results)
-		results = sorted(results, key=lambda tuple: tuple[1])
-		if results:
-			return results[0][1]
+		f_results = filter(None, results)
+		s_results = sorted(f_results, key=lambda duple: duple[0])
+		if s_results:
+			return s_results[0][1]
 		else:
 			return self.background
-
-
 
 	def traceSphere(self,ray,sphere):
 		# e = eye, cam pos
@@ -390,38 +313,32 @@ class Raytracer:
 
 		# n = sphere normal, radius is length
 		n = sphere.normal
-		radius = math.sqrt(n[0]**2 + n[1]**2 + n[2]**2)
+		radius = math.sqrt(self.dot(n, n))
 
 	  # simplify below calculations
 		e_minus_c = ( e[0] - c[0], e[1] - c[1], e[2] - c[2] )
 		d_dot_d   = self.dot(d,d)
 
 		# discriminant to see if we hit 
-		discriminant = (self.dot(d, e_minus_c))**2 -
-		 							 (d_dot_d * (self.dot(e_minus_c, e_minus_c) - (radius**2)))
+		discriminant = (self.dot(d, e_minus_c))**2 - (d_dot_d * (self.dot(e_minus_c, e_minus_c) - (radius**2)))
 
-		
 		if discriminant >= 0:
-			discriminant = discriminant**.5
-			neg_d = []
-			for i in range(len(d)):
-				neg_d.append(d[i]*-1)
+			neg_d = (-1 * d[0], -1 * d[1], -1 * d[2])
 
-			t_plus  = (numpy.dot(neg_d,e_minus_c) + discriminant)/d_dot_d
-			t_minus = (numpy.dot(neg_d,e_minus_c) - discriminant)/d_dot_d
+			t_plus  = (self.dot(neg_d,e_minus_c) + math.sqrt(discriminant))/d_dot_d
+			t_minus = (self.dot(neg_d,e_minus_c) - math.sqrt(discriminant))/d_dot_d
 
 			tmin = min(t_plus,t_minus)
 			tmax = max(t_plus,t_minus)
 			
 			if tmin >= 0:
-				return tmin
+				return (tmin, sphere.ambientMaterial.value)
 			elif tmax >= 0:
-				return tmax
+				return (tmax, sphere.ambientMaterial.value)
 			else:
 				return False
 		else:	
 			return False
-
 
 trace = Raytracer(scene)
 trace.parseFile()
